fc3671349c764f01d68df113753ae532c0d7c554
Mash94 <nico.mahnic94@gmail.com>
2020-07-22 20:15:29 -0300

    min 48.20

--- src/App.js
+++ src/App.js
@@ -2,7 +2,7 @@ import React, {useState, useEffect} from 'react';
 import './App.css';
 import getGif from './services/getGif'
 
-function App() {
+function App() {  // min 48.20
   const [gifs, setGifs] = useState([])
 
   useEffect(() => {         
