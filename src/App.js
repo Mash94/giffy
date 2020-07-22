a42d5ebcb1bc7d15c986a9fbe2f2bb66406f45b2
Mash94 <nico.mahnic94@gmail.com>
2020-07-22 18:06:18 -0300

    export getGif

--- src/App.js
+++ src/App.js
@@ -1,35 +1,25 @@
 import React, {useState, useEffect} from 'react';
 import './App.css';
-
-const GIFS = [
-  'https://media3.giphy.com/media/EPcvhM28ER9XW/200w.webp?cid=ecf05e47spcm71hlbd9eupshys5slouenzsyc9r7uvr0r4zr&rid=200w.webp',
-  'https://media1.giphy.com/media/l378vMO5yhVuap3pe/200w.webp?cid=ecf05e47s5maedwmxx2vpnbligdsckjquop5g3ps912mj85h&rid=200w.webp'
-]
-const DIFFERENT_GIFS = [
-  'https://media3.giphy.com/media/tz6S94VbrauGs/giphy.gif?cid=ecf05e47uo64abl2o36fwy7cey2q7vp1m5t8s1sc7heidykg&rid=giphy.gif',
-  'https://media2.giphy.com/media/1J4X5chVxtBFS/giphy.gif?cid=ecf05e47cae6974ef6fbadd5949bfeac5e1f66afc5e4d864&rid=giphy.gif'
-]
+import getGif from './services/getGif'
 
 function App() {
-  const [gifs, setGifs] = useState(GIFS)
+  const [gifs, setGifs] = useState([])
 
   useEffect(() => {         
     // por defecto useEffect se ejecuta cada vez que se re-renderiza el componente
-      console.log('Efecto')
+      //console.log('Efecto')
+      const keyword = 'evangelion'
+      const limit = 2
+      getGif({keyword, limit}).then(gifs => setGifs(gifs))
     }, [])  // Como segundo paramentro a useEffect se le pasan las dependencias
     // Las dependencias son variables o informacion que se cambia debe ejecutarse este efecto
     // con dependencias vacias se ejecuta una sola vez EQU a ComponentDidMount()
   
   return (
-    <div className='App'>
-    <section className='App-content'>
-      {gifs.map((singleGif,keyGif) => (<img key={keyGif} src={singleGif} />))}
-      <button 
-        className='btn btn-primary'
-        onClick={() => setGifs(DIFFERENT_GIFS)} //min 25.05
-        >Cambiar Gifs
-      </button>
-    </section> 
+    <div className='App-content'>
+      {gifs.map((singleGif) => 
+        <img key={singleGif.id} alt={'imagen'+singleGif.id} src={singleGif.image} />
+      )}
     </div>
   );
 }
---
+++ src/services/getGif.js
@@ -0,0 +1,17 @@
+const apiKEY = 'm0JTEDIpPzZTMKm1pCJ8839ngXzt5sIi'
+
+export default function getGif({keyword = 'trends', limit}){
+  console.log(keyword)
+  const apiURL = 'https://api.giphy.com/v1/gifs/search?api_key='+apiKEY+'&q='+keyword+'&limit='+limit+'&offset=0&rating=g&lang=en'
+
+  return fetch(apiURL)
+    .then(res => res.json())
+    .then(response => {
+      const {data} = response
+      const gifs = data.map(image => {return{
+        image: image.images.downsized_medium.url,
+        id: image.id
+      }})
+      return gifs
+    })
+}
