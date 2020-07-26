c47fd866b23676e1a9e0f0418303fe9e6d4ac313
Mash94 <nico.mahnic94@gmail.com>
2020-07-25 21:08:45 -0300

    State to ListOfGifs

--- src/App.js
+++ src/App.js
@@ -1,26 +1,13 @@
-import React, {useState, useEffect} from 'react';
+import React from 'react';
 import './App.css';
-import getGif from './services/getGif'
 import ListOfGifs from './component/ListOfGifs'
 
 function App() {  // min 48.20
-  const [gifs, setGifs] = useState([])
 
-  useEffect(() => {         
-    // por defecto useEffect se ejecuta cada vez que se re-renderiza el componente
-      //console.log('Efecto')
-      const GIF_params = {
-        keyword: 'programing',
-        limit: 5
-      }
-      getGif(GIF_params).then(gif => setGifs(gif))
-    }, [])  // Como segundo paramentro a useEffect se le pasan las dependencias
-    // Las dependencias son variables o informacion que se cambia debe ejecutarse este efecto
-    // con dependencias vacias se ejecuta una sola vez EQU a ComponentDidMount()
-  
   return (
     <div className='App-content'>
-      <ListOfGifs gifs={gifs}/>      
+      <ListOfGifs keyword='belguim' limit={2}/>
+      <ListOfGifs keyword='chile' limit={3}/>      
     </div>
   );
 }
--- src/component/ListOfGifs.jsx
+++ src/component/ListOfGifs.jsx
@@ -1,7 +1,17 @@
-import React from 'react'
+import React, {useState, useEffect} from 'react'
 import Gif from './Gif'
+import getGif from '../services/getGif'
 
-export default function ListOfGifs({gifs}){
+
+export default function ListOfGifs({keyword='panda', limit=10}){
+  const [gifs, setGifs] = useState([])
+
+  useEffect(() => {        // por defecto useEffect se ejecuta cada vez que se re-renderiza el componente 
+    getGif({keyword, limit}).then(gif => setGifs(gif))
+  }, [keyword,limit])  // Como segundo paramentro a useEffect se le pasan las dependencias
+  // Las dependencias son variables o informacion que se cambia debe ejecutarse este efecto
+  // con dependencias vacias se ejecuta una sola vez EQU a ComponentDidMount()
+  
   return  (
     gifs.map((singleGif) => <Gif key={singleGif.id} {...singleGif} />) 
   )    
